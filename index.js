b89a25196048e57f37122e23c6885908a68436a5
mdmehadihassanofficial <coexactlast@gmail.com>
2025-05-31 00:33:04 +0600

    Final Submit

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const fetchData = async () => {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const posts = await response.json();
 
-        posts.slice(0, 10).forEach(post => {
+        posts.slice(0, 9).forEach(post => {
             const col = document.createElement("div");
             col.className = "col-12 col-md-6 col-lg-4 mb-4";
 
             col.innerHTML = `
-                <div class="card" style="width: 100%;">
+                <div class="card blog-card" style="width: 100%;">
                     <article class="card-body">
                         <h5 class="card-title">${post.title}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Post ID: ${post.id}</h6>
